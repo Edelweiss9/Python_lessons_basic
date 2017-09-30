3cf434fce629f184c09096f2578ac066663fb17c
Alex <bumblbee13@icloud.com>
2017-09-30 06:43:24 +0300

    hw 03 ez+norm
    Add easy and norm 03 hw

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -5,7 +5,22 @@
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
 def my_round(number, ndigits):
-    pass
+    frac = number % 1 * (10 ** ndigits)
+    rem = frac % 1
+
+    if rem * 2 >= 1:
+        rem = 1
+    else:
+        rem = 0
+
+    frac = frac // 1 + rem
+
+    if int(frac) == 100000:
+        number = str(int(number // 1 + 1)) + ('.' + str((frac / (10 ** ndigits)))[2:])
+    else:
+        number = str(int(number // 1)) + ('.' + str((frac / (10 ** ndigits)))[2:])
+
+    return float(number)
 
 
 print(my_round(2.1234567, 5))
@@ -20,7 +35,8 @@ print(my_round(2.9999967, 5))
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
 def lucky_ticket(ticket_number):
-    pass
+    numbers = [int(n) for n in str(ticket_number)]
+    return True if sum(numbers[:3]) == sum(numbers[3:]) else False
 
 
 print(lucky_ticket(123006))
--- lesson03/home_work/hw03_normal.py
+++ lesson03/home_work/hw03_normal.py
@@ -2,8 +2,16 @@
 # Напишите функцию, возвращающую ряд Фибоначчи с n-элемента до m-элемента.
 # Первыми элементами ряда считать цифры 1 1
 
-def fibonacci(n, m):
-    pass
+def Fibonacci(n, m):
+    a, b = 0, 1
+    for i in range(m + 1):
+        if i >= n:
+            yield a
+        a, b = b, a + b
+
+
+print(*Fibonacci(5, 21))
+
 
 # Задача-2:
 # Напишите функцию, сортирующую принимаемый список по возрастанию.
@@ -12,16 +20,42 @@ def fibonacci(n, m):
 
 
 def sort_to_max(origin_list):
-    pass
+    if len(origin_list) <= 1:
+        return origin_list
+    else:
 
-sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
+        go = 1
+        while go < len(origin_list):
+            for i in range(len(origin_list) - go):
+                if origin_list[i] > origin_list[i + 1]:
+                    origin_list[i], origin_list[i + 1] = origin_list[i + 1], origin_list[i]
+            go += 1
+
+    return origin_list
+
+
+print(sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0]))
+
 
 # Задача-3:
 # Напишите собственную реализацию стандартной функции filter.
 # Разумеется, внутри нельзя использовать саму функцию filter.
+
+def one_more_filter(func, data):
+    return [i for i in data if func(i) == True]
 
 
 # Задача-4:
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
 # Определить, будут ли они вершинами параллелограмма.
 
+def is_para(a1, a2, a3, a4):
+    return True if a1[1] == a4[1] or a2[1] == a3[1] and (abs(a1[0] - a2[0]) == abs(a3[0] - a4[0])) else False
+
+
+c1 = (2, 2)
+c2 = (2, 8)
+c3 = (-9, 5)
+c4 = (-8, 2)
+
+print(is_para(c1, c2, c3, c4))
