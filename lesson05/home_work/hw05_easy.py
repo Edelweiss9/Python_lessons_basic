221a5ed7c1100ea616203f03c2d8884fa78d1dff
Alex <bumblbee13@icloud.com>
2017-10-10 06:24:54 +0300

    ez05 + norm05
    Add easy05, draft normal05

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -3,8 +3,76 @@
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
+import os
+import shutil
+
+# if __name__ == "__main__":
+#     pass
+
+# Создание папок вида dir_n
+
+def folderNumCreator(n):
+    for i in range(0, n):
+        try:
+            os.mkdir(os.path.join(os.getcwd(), 'dir_{}'.format(i)))
+            print('Folder dir_{} created.'.format(i))
+        except FileExistsError:
+            print('Folder dir_{} already exists!'.format(i))
+            continue
+
+
+# Удаление n-папок вида dir_n от меньшего имени
+
+def folderNumDeleter(n):
+    for i in range(0, n):
+        try:
+            shutil.rmtree(os.path.join(os.getcwd(), 'dir_{}'.format(i)))
+        except NameError:
+            print('Folder dir_{} does not exist!.'.format(i))
+
+
+# Удаление конкретной папки
+
+def specFolderDeleter(dir):
+    try:
+        shutil.rmtree(os.path.join(os.getcwd(), '{}'.format(dir)))
+        print('Folder "{}" deleted!'.format(dir))
+    except FileNotFoundError as err:
+        print(err)
+
+
+# Создание конкретной папки в текущей директории
+
+def specFolderCreator(dir):
+    try:
+        os.mkdir(dir)
+        print('Folder "{}" created'.format(dir))
+    except FileExistsError as err:
+        print(err)
+
+
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 
+def listFolders():
+    dir_list = next(os.walk('.'))[1]
+    print(dir_list)
+
+
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
+def copyCurrentFile():
+    new = __file__ + '.copy.py'
+    shutil.copy2(os.path.abspath(__file__), new)
+
+
+# -----------------------------------------------------------------------------
+# Export:
+
+def pathChanger(new):
+    try:
+        os.chdir(new)
+        print('Changed dir to: ' + os.getcwd())
+    except FileNotFoundError:
+        print('Dir "{}" not found!'.format(new))
--- lesson05/home_work/hw05_normal.py
+++ lesson05/home_work/hw05_normal.py
@@ -13,3 +13,63 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
+
+import os
+import sys
+import shutil
+import hw05_easy as lib
+
+print('sys.argv = ', sys.argv)
+
+
+def helpCall():
+    print('"cd" - change directory: cd <path> or cd <folder_name> if folder is in current dir')
+    print('"li" - list folders in current directory')
+    print('"df" - delete folder: df <folder_name>')
+    print('"cf" - create folder: cf <folder_name>')
+    print('"help" - available commands')
+    print('"q" - exit')
+
+
+def main():  # main(command, arg) - ?
+    do = {
+        'cd': lib.pathChanger,
+        'li': lib.listFolders,
+        'df': lib.specFolderDeleter,
+        'cf': lib.specFolderCreator,
+        'help': helpCall,
+        'q': sys.exit
+    }
+
+    print('What do you want me to do, human?')
+    print(list(do))
+    answer = str(input())
+    try:
+        key = sys.argv[1]
+    except IndexError:
+        key = None
+    if answer in list(do):  # argv должен быть в словаре do. как проверить? аргв должен проверяться по input?
+        do.get(key)
+    else:
+        print('Invalid input, type "help" to get all available commands')
+
+# 51: slice с первой части инпута, а не весь? сравнить с листом => выдать соотв функцию == из словаря
+
+if __name__ == "__main__":
+    main()
+
+# try:
+#     dir_name = sys.argv[2]
+# except IndexError:
+#     dir_name = None
+#
+# try:
+#     key = sys.argv[1]
+# except IndexError:
+#     key = None
+#
+# if key:
+#     if do.get(key):
+#         do[key]()
+#     else:
+#         print('Invalid input, type "help" to get all available commands')
