5da148db05638def1b7d0457510ab357319ca3bf
Alex <bumblbee13@icloud.com>
2017-10-14 16:48:06 +0300

    hw06_norm unfinished
    Add fixes 06_norm. Tbd.

--- lesson06/home_work/hw06_normal.py
+++ lesson06/home_work/hw06_normal.py
@@ -33,19 +33,29 @@ class Pupil(Human):
         self.otousan = otousan
         self.okaasan = okaasan
 
+    # Отдельно функцию для родителей (несовпадающие фамилии)
+
     def __str__(self):
+        return '{} {}, {}, {}'.format(self.last_name, self.first_name,
+                                      self.otousan, self.okaasan)
+
+    def __repr__(self):
         return '{} {}, {}, {}'.format(self.last_name, self.first_name,
                                       self.otousan, self.okaasan)
 
 
 class Teacher(Human):
-    def __init__(self, id, last_name, first_name, subject):
+    def __init__(self, id, last_name, first_name, subjects):
         Human.__init__(self, id, last_name, first_name)
-        self.subject = subject
+        self.subjects = subjects
 
     def __str__(self):
         return '{} {}, {}'.format(self.last_name, self.first_name,
-                                  self.subject)
+                                  self.subjects)
+
+    def __repr__(self):
+        return '{} {}, {}'.format(self.last_name, self.first_name,
+                                  self.subjects)
 
 
 class Subject:
@@ -54,7 +64,10 @@ class Subject:
         self.subject = subject
 
     def __str__(self):
-        return '{}'.self.subject
+        return '{}'.format(self.subject)
+
+    def __repr__(self):
+        return '{}'.format(self.subject)
 
 
 class Grade:
@@ -65,6 +78,9 @@ class Grade:
         self.teachers = teachers
 
     def __str__(self):
+        return '{}. {}. {}.'.format(self.grade, self.pupils, self.teachers)
+
+    def __repr__(self):
         return '{}. {}. {}.'.format(self.grade, self.pupils, self.teachers)
 
 
@@ -92,30 +108,72 @@ class School:
     def show_subjects_of_pupil(self, pupil_id):
         grade = self.pupils[pupil_id].grade
         teachers = self.grades[grade].teachers
-        for t in teachers:
-            print(teachers[t].subjects)
+        for i in teachers:
+            print(teachers[i].subjects)
 
 
-school = School('Konoha Ninja Academy', teachers, grades, pupils)
+sub1 = Subject('id_ninjutsu', 'Ninjutsu')
+sub2 = Subject('id_ninja_history', 'Ninja_history')
+sub3 = Subject('id_genjutsu', 'Genjutsu')
 
 t0 = Teacher(1, 'Hatake', 'Kakashi', {sub1.id: sub1})
 t1 = Teacher(2, 'Sarutobi', 'Hiruzen', {sub2.id: sub2})
 t2 = Teacher(3, 'Uchiha', 'Izuna', {sub3.id: sub3})
 
+p0 = Pupil(4, 'Uzumaki', 'Naruto', Human(10, 'last_name', 'otousan'),
+           Human(11, 'last_name', 'okaasan'), 'grade_7')
+p1 = Pupil(5, 'Uchiha', 'Sasuke', Human(12, 'last_name', 'otousan'),
+           Human(13, 'last_name', 'okaasan'), 'grade_7')
+p2 = Pupil(6, 'Inuzuka', 'Kiba', Human(14, 'last_name', 'otousan'),
+           Human(15, 'last_name', 'okaasan'), 'grade_8')
+p3 = Pupil(7, 'Aburame', 'Shino', Human(16, 'last_name', 'otousan'),
+           Human(17, 'last_name', 'okaasan'), 'grade_9')
+
+grade_7 = Grade(77, 'Grade 7', {p0.id: p0, p1.id: p1}, {t0.id: t0})
+grade_8 = Grade(88, 'Grade 8', {p1.id: p1, p2.id: p2}, {t1.id: t1, t2.id: t2})
+grade_9 = Grade(99, 'Grade 9', {p2.id: p2, p3.id: p3}, {t2.id: t2})
+
 subjects = {
     'id_ninjutsu': sub1,
     'id_ninja_history': sub2,
-    'id_genjutsu': sub3
+    'id_genjutsu': sub3,
 }
 
-p0 = Pupil(4, 'Uzumaki', 'Naruto', Human(id, 'last_name', 'otousan'),
-           Human(id, 'last_name', 'okaasan'), 'grade')
+teachers = {}
+parents = {}
 
+grades = {
+    grade_7.id: grade_7,
+    grade_8.id: grade_8,
+    grade_9.id: grade_9,
+}
 
+pupils = {
+    p0.id: p0,
+    p1.id: p1,
+    p2.id: p2,
+    p3.id: p3,
+}
+
+school = School('Konoha Ninja Academy', teachers, grades, pupils)
+
+print('Grades:\n', school.grades)
+print('***')
+# print('Pupils in grade 7:\n')
+# school.display_pupils('grade_7')
+print('***')
+# school.show_subjects_of_pupil(4)
+print('***')
+# print(p0.otousan)
+# print(p0.okaasan)
+print('***')
+# print(school.display_teachers_grades('grade_7'))
+#
+#
 # ***Unfinished***
 #
-# 0) Grades + grades list + grade.grade check
-# 1) Pupils list
-# 2) Parents surname check
-# 3) ID!
-# 4) Names similarity in functions check
+# 0) Parents names as dict?
+# 1) ID
+# 2) KeyError 104, 161
+# 3)
+# 4)
