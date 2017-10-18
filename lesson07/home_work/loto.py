c5823d945bb4796a83779642119925eba8ae4d85
Alex <bumblbee13@icloud.com>
2017-10-18 09:45:34 +0300

    hw07 without row
    Add hw_07 unfinished

--- lesson07/home_work/loto.py
+++ lesson07/home_work/loto.py
@@ -57,3 +57,181 @@
 модуль random: http://docs.python.org/3/library/random.html
 
 """
+
+import random
+import sys
+
+
+# Так и не получилось прикрутить проверку ряда на == 'X'
+# Вернее в какой-то момент получилось, захотел сделать красивее - и доломал :(
+
+class Gambler(object):
+    def __init__(self):
+        self.card = Card()
+        self.points = 0
+
+    def cross(self, number=None):
+        if self.check_number(number):
+            print('''Crossed out!
+    AI is playing...''')
+            self.points += 1
+            self.card.update(number)
+        else:
+            print('''No such number in your card!
+    ~~~GAME OVER!~~~
+            ''')
+            sys.exit()
+
+    def next(self, number=None):
+        if self.check_number(number):
+            print('''You missed the number! Be more attentive next time!
+             ~~~GAME OVER!~~~
+                  ''')
+            sys.exit()
+        else:
+            pass
+
+    def check_number(self, number):
+        line = self.card.store_card()
+        for seq in line:
+            for el in seq:
+                if el == number:
+                    return True
+        return False
+
+
+class Human(Gambler):
+    def __init__(self):
+        super().__init__()
+        self.card.print_new_card()
+
+    def make_turn(self, number=None):
+        print('**** YOUR CARD ****')
+        self.card.show_card()
+        print('''      Getting number!
+        *rolling*
+         ******* 
+          ***** 
+           ***''')
+        print('Number is: ', number)
+        print('''What now?
+    "x" to cross out the number
+    "n" to draw next number 
+    "q" to end the game''')
+
+        turn = input('')
+        if turn == 'x':
+            self.cross(number)
+        if turn == 'n':
+            self.next(number)
+        if turn == 'q':
+            print('NO WINNER TODAY! Thanks for playing and have a nice day!')
+            sys.exit()
+        while turn not in ('x', 'n', 'q'):
+            turn = input('Invalid input!')
+
+
+class Computer(Gambler):
+    def __init__(self):
+        super().__init__()
+        self.card.print_new_card()
+
+    def make_turn(self, number=None):
+        print('***** AI CARD *****')
+        if self.check_number(number):
+            self.points += 1
+        self.card.update(number)
+        self.card.show_card()
+
+
+class Card(object):
+    def __init__(self):
+        self.all_cards = [x for x in range(1, 91)]
+        self.numbers_max = 15
+        self.card = self.print_new_card()
+
+    def print_new_card(self):
+        c = 0
+        self.random_numbers = random.sample(self.all_cards, self.numbers_max)
+        self.card = [['' for x in range(9)] for i in range(3)]
+        for i in range(len(self.card)):
+            for j in range(len(self.card[i])):
+                if j <= 4:
+                    self.card[i][j] = self.random_numbers[c]
+                    c += 1
+        for i in range(len(self.card)):
+            random.shuffle(self.card[i])
+        return self.card
+
+    def store_card(self, number=None):
+        self.c = self.card[:]
+        if self.update(number):
+            self.c = self.update(number)
+        return self.c
+
+    # def check_row(self):
+    #     self.c = self.card[:]
+    #     for x in self.c:
+    #         if list(filter(None, x))
+    #
+    #
+    #         if all(list(filter(None, list))) == 'X':
+    #             return True
+
+    # def check_equal(self):
+    #     list =
+    #     return list[1:] == list[:-1]
+
+
+    def show_card(self):
+        for i in self.store_card():
+            print(' '.join(map(str, i)))
+        print('*******************')
+
+    def update(self, number):
+        for seq in self.card:
+            for el in range(len(seq)):
+                if seq[el] == number:
+                    seq[el] = 'X'
+        return self.card
+
+
+class NumbersBank(object):
+    def __init__(self):
+        self.pool = [x for x in range(1, 91)]
+
+    def roll(self):
+        pick = random.choice(self.pool)
+        self.pool.remove(pick)
+        return pick
+
+
+if __name__ == '__main__':
+    print('LOTTERY 5/90! v. 1.0')
+    answer = input('Wanna play? Type "y" or "n":')
+    if answer == 'n':
+        print('Maybe another time! See you in Vegas!')
+        sys.exit()
+    while answer not in ('y', 'n'):
+        answer = input('Invalid input!')
+
+    bank = NumbersBank()
+    player = Human()
+    AI = Computer()
+    card = Card()
+
+    while True:
+        num = bank.roll()
+        player.make_turn(num)
+        AI.make_turn(num)
+        print('Your points: ', player.points)
+        print('AI points: ', AI.points)
+        # if card.check_row():
+        #     print('Row crossed out! Game finished!')
+        #     sys.exit()
+        if player.points == 5:
+            print('YOU WON!')
+            sys.exit()
+        elif AI.points == 5:
+            print('AI WON!')
+            sys.exit()
